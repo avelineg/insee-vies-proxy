6ae0b6cf26271618e470fb053810acd267bc6e07
avelineg <a.guenneugues@cmexpert.fr>
2025-06-24 14:13:44 +0200

    Add files via upload

---
+++ api/vies.js
@@ -0,0 +1,23 @@
+// Fichier: api/vies.js
+
+export default async function handler(req, res) {
+  const { country, vat } = req.query;
+
+  if (!country || !vat) {
+    return res.status(400).json({ error: 'Paramètres manquants: country et vat requis' });
+  }
+
+  try {
+    const apiUrl = `https://ec.europa.eu/taxation_customs/vies/rest-api/ms/${country}/vatnumber/${country}${vat}`;
+    const response = await fetch(apiUrl);
+
+    if (!response.ok) {
+      return res.status(response.status).json({ error: 'Erreur de l’API VIES' });
+    }
+
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: 'Erreur serveur: ' + error.message });
+  }
+}
