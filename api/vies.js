2905928738194ca79aff53be4494685fda28ba1e
avelineg <a.guenneugues@cmexpert.fr>
2025-06-24 15:07:52 +0200

    Update vies.js

--- api/vies.js
+++ api/vies.js
@@ -1,7 +1,5 @@
-// Fichier: api/vies.js
-
+// api/vies.js
 import axios from 'axios';
-import { XMLParser } from 'fast-xml-parser';
 
 export default async function handler(req, res) {
   const { country, vat } = req.query;
@@ -10,49 +8,33 @@ export default async function handler(req, res) {
     return res.status(400).json({ error: 'Paramètres manquants: country et vat requis' });
   }
 
-  const soapEnvelope = `
-    <soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"
-                   xmlns:tns="urn:ec.europa.eu:taxud:vies:services:checkVat:types">
-      <soap:Body>
-        <tns:checkVat>
-          <tns:countryCode>${country}</tns:countryCode>
-          <tns:vatNumber>${vat}</tns:vatNumber>
-        </tns:checkVat>
-      </soap:Body>
-    </soap:Envelope>
-  `;
+  const xml = `
+    <soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:urn="urn:ec.europa.eu:taxud:vies:services:checkVat:types">
+      <soapenv:Header/>
+      <soapenv:Body>
+        <urn:checkVat>
+          <urn:countryCode>${country}</urn:countryCode>
+          <urn:vatNumber>${vat}</urn:vatNumber>
+        </urn:checkVat>
+      </soapenv:Body>
+    </soapenv:Envelope>`;
 
   try {
-    const { data } = await axios.post(
+    const response = await axios.post(
       'https://ec.europa.eu/taxation_customs/vies/services/checkVatService',
-      soapEnvelope,
+      xml,
       {
         headers: {
-          'Content-Type': 'text/xml; charset=utf-8',
+          'Content-Type': 'text/xml;charset=UTF-8',
           'SOAPAction': ''
-        },
-        timeout: 10000
+        }
       }
     );
 
-    const parser = new XMLParser({
-      ignoreAttributes: false,
-      attributeNamePrefix: '@_',
-    });
-
-    const json = parser.parse(data);
-
-    const result =
-      json['soap:Envelope']?.['soap:Body']?.['checkVatResponse'] ??
-      json['soap:Envelope']?.['soap:Body']?.['ns2:checkVatResponse'];
-
-    if (!result) {
-      throw new Error('Réponse invalide du service VIES');
-    }
-
-    res.status(200).json(result);
+    const result = response.data;
+    res.status(200).send(result); // ou parsage XML -> JSON si tu veux
   } catch (error) {
-    console.error('Erreur VIES :', error.message);
+    console.error('Erreur SOAP :', error.message);
     res.status(500).json({ error: 'Erreur lors de la vérification TVA' });
   }
 }
